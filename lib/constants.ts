d27ba886242fe42f58ddb33ae0f2d60630085110
AlphaGHX <ChenYuanHangOL@outlook.com>
2022-02-24 17:31:54 +0800

    feat: 配置了代理，解决跨域问题

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,10 @@
 export const REMOTE = {
-  ROOT: 'http://localhost:12900/',
-  LIST: 'http://localhost:12900/system/list',
-  LIST_IMG: 'http://localhost:12900/system/list/img/',
-  BLOG: 'http://localhost:12900/system/blog/',
-  BLOG_INFO: 'http://localhost:12900/system/blog-info/',
-  TAG: 'http://localhost:12900/system/tag/',
-  VERIFY_TOKEN: 'http://localhost:12900/system/token-verify',
-  VERIFY_USER: 'http://localhost:12900/system/user-verify',
+  ROOT: 'http://localhost:3000/api/',
+  LIST: 'http://localhost:3000/api/system/list',
+  LIST_IMG: 'http://localhost:3000/api/system/list/img/',
+  BLOG: 'http://localhost:3000/api/system/blog/',
+  BLOG_INFO: 'http://localhost:3000/api/system/blog-info/',
+  TAG: 'http://localhost:3000/api/system/tag/',
+  VERIFY_TOKEN: 'http://localhost:3000/api/system/token-verify',
+  VERIFY_USER: 'http://localhost:3000/api/system/user-verify',
 }
